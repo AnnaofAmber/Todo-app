7623d01e8634f3dee1d0f1446cc2390e08ec53c3
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-27 20:43:53 +0300

    fixing compiling problem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { CreateTaskForm } from "./CreateTaskForm/CreateTaskForm";
 import { Layout } from "./Layout/Layout";
 import { TasksList } from "./TasksList/TasksList";
-import { getError, getIsLoading } from "redux/selectors";
+// import { getError, getIsLoading } from "redux/selectors";
 import { useEffect } from "react";
 import { fetchTasks } from "redux/operations";
 import { StatusFilter } from "./StatusFilter/StatusFilter";
@@ -10,8 +10,8 @@ import { AppBar } from "AppBar/AppBar";
 
 export const App = () => {
  const dispatch = useDispatch()
- const isLoading = useSelector(getIsLoading);
- const error = useSelector(getError);
+//  const isLoading = useSelector(getIsLoading);
+//  const error = useSelector(getError);
 
  useEffect(()=>{
 dispatch(fetchTasks())
