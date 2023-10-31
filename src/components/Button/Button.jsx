22b401e5101e5a2e2ee4c2a89dc1b67162217063
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-31 21:19:47 +0200

    correcting dark styles for filter

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,14 @@
 import clsx from 'clsx';
 import scss from './Button.module.scss'
+import { useSelector } from 'react-redux';
+import { getTheme } from 'redux/selectors';
 
 export const Button = ({selected = false, type = 'button', children, ...otherProps}) => {
+  const theme = useSelector(getTheme)
   return (
     <button
     className={clsx(scss.status, {
-      [scss.isSelected]: selected
+      [scss.isSelected]: selected,[scss.dark]:theme
     })}
       type={type}
       {...otherProps}
