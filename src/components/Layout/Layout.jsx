b4b12dd32e2f07a50f070c497e5a558dca5eebe8
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-31 20:49:41 +0200

    adding styles for header + filter

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,13 +7,14 @@ import clsx from "clsx"
 
 export const Layout = ({children})=>{
     const theme = useSelector(getTheme)
-    console.log(theme);
     const dispatch = useDispatch()
     const handleThemeChange = theme => dispatch(setTheme(theme))
     return(
-<div className={clsx(scss.container, {
+<div className={clsx(scss.wrapper, {
     [scss.dark]:theme
 })}>
+    <div className={scss.hero}></div>
+    <div className={scss.container}>
 <header className={scss.header}> 
         <h1 className={scss.title}>
             Todo
@@ -23,6 +24,7 @@ export const Layout = ({children})=>{
         <main >
 
             {children}</main>
+</div>
 </div>
     )
 }
--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,17 +1,21 @@
-// import scss from "./StatusFilter.scss"
+import scss from "./StatusFilter.module.scss"
+import clsx from "clsx"
 import { Button } from "components/Button/Button"
 import { useDispatch, useSelector } from "react-redux"
 import { statusFilters } from "redux/constants"
 import { setStatusFilter } from "redux/filterStatusSlice"
-import { getFilterStatus } from "redux/selectors"
+import { getFilterStatus, getTheme } from "redux/selectors"
 
 export const StatusFilter = () =>{
 const dispatch = useDispatch()
 const filter = useSelector(getFilterStatus)
 const handleFilterChange = filter => dispatch(setStatusFilter(filter))
+const theme = useSelector(getTheme)
 
     return(
-        <div>
+        <div className={clsx(scss.wrapper, {
+            [scss.dark]:theme
+        })}>
 <Button  selected={filter === statusFilters.all} onClick={()=>handleFilterChange(statusFilters.all)}>All</Button>
 <Button selected={filter === statusFilters.active} onClick={()=>handleFilterChange(statusFilters.active)}>Active</Button>
 <Button selected={filter === statusFilters.completed} onClick={()=>handleFilterChange(statusFilters.completed)}>Completed</Button>
