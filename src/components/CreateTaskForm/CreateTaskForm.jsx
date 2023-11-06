9398b5800fe4b04a7f4b35a5590a8036eb859783
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-11-06 20:09:18 +0200

    added dark styles for form

--- src/components/CreateTaskForm/CreateTaskForm.jsx
+++ src/components/CreateTaskForm/CreateTaskForm.jsx
@@ -1,10 +1,13 @@
+import clsx from "clsx"
 import scss from "./CreateTaskForm.module.scss"
 
 import { useDispatch } from "react-redux"
 import { addTask } from "redux/operations"
+import { useSelector } from "react-redux"
+import { getTheme } from "redux/selectors"
 
 export const CreateTaskForm = () =>{
-
+const theme = useSelector(getTheme)
 const dispatch = useDispatch()
 const hadnleSubmit = e => {
     e.preventDefault()
@@ -17,8 +20,12 @@ const hadnleSubmit = e => {
 
         <form className={scss['form-add']} onSubmit={hadnleSubmit}>
             <label htmlFor="createTask"></label>
-            <input className={scss['input-add']} type="text" id="createTask" name="text" placeholder="Create a new todo..."/>
-            <button className={scss['button-add']} ></button>
+            <input className={clsx(scss['input-add'], {
+            [scss.dark]:theme
+        })} type="text" id="createTask" name="text" placeholder="Create a new todo..."/>
+            <button className={clsx(scss['button-add'], {
+            [scss.dark]:theme
+        })} ></button>
         </form>
     )
 }
